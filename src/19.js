314d08d89fd8431643767b050830882176cac5f6
louphaskew737 <louphaskew737@gmail.com>
2025-03-29 06:07:11 +0000

    Add 19.js

---
+++ src/19.js
@@ -0,0 +1,9 @@
+function displayMenu() {
+  console.log("Please select an option:");
+  console.log("1. Add item to list");
+  console.log("2. Remove item from list");
+  console.log("3. Update item in list");
+  console.log("4. Exit program");
+}
+
+displayMenu();
