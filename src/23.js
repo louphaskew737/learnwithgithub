a5d76f675fe51e99629d3024fdaf8d3894301d88
louphaskew737 <louphaskew737@gmail.com>
2025-04-03 12:40:25 +0000

    Add 23.js

---
+++ src/23.js
@@ -0,0 +1,8 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage:
+
+let randomNumber = getRandomInt(10);
+console.log(randomNumber); // Output can vary between 1 and 9
